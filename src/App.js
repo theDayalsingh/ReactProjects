a2199522ebab22993b01eb479c7992992e6a8c07
theDayalsingh <thedayalsinghgmail.com>
2023-05-11 19:00:54 +0530

    Ecomm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavbarHeader from './Components/Layout/Navbar';
+import Cardbootstrap from './Components/UI/Card';
+import Footer from './Components/Layout/Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarHeader/>
+      <Cardbootstrap/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
